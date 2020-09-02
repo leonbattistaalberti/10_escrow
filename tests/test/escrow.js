3d686cd89d1234df83bcb75967d0e1693bd34e40
thelonehegelian <thelonehegelian@gmail.com>
2020-09-02 09:58:34 +0100

    Pass should deposit test

--- tests/test/escrow.js
+++ tests/test/escrow.js
@@ -1,19 +1,24 @@
-const Escrow = artifacts.require('Escrow');
+const Escrow = artifacts.require("Escrow");
 
 const assertError = async (promise, error) => {
   try {
     await promise;
-  } catch(e) {
-    assert(e.message.includes(error))
+  } catch (e) {
+    assert(e.message.includes(error));
     return;
   }
   assert(false);
-}
+};
 
-contract('Escrow', () => {
+contract("Escrow", (accounts) => {
   let escrow = null;
+  const [lawyer, payer, payee] = accounts;
   before(async () => {
     escrow = await Escrow.deployed();
   });
+  it("Should deposit", async () => {
+    await escrow.deposit({ from: payer, value: 900 });
+    const deployerBalance = parseInt(await web3.eth.getBalance(escrow.address));
+    assert.equal(deployerBalance, 900);
+  });
 });
-
